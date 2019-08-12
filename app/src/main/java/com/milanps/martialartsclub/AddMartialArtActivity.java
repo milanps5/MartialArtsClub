c0141f6c48ee23e1933d15b02b398ef4c0611050
milan <milanps92@gmail.com>
2019-08-12 20:48:46 +0200

    edit texts and buttons

--- app/src/main/java/com/milanps/martialartsclub/AddMartialArtActivity.java
+++ app/src/main/java/com/milanps/martialartsclub/AddMartialArtActivity.java
@@ -2,15 +2,30 @@ package com.milanps.martialartsclub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class AddMartialArtActivity extends AppCompatActivity {
+
+    EditText edtName;
+    EditText edtPrice;
+    EditText edtColor;
+    Button btnAddMartialArt;
+    Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_martial_art);
 
-        Toast.makeText(AddMartialArtActivity.this, "This is add Martial Art Activity", Toast.LENGTH_SHORT).show();
+        edtName = (EditText) findViewById(R.id.edtName);
+        edtPrice = (EditText) findViewById(R.id.edtPrice);
+        edtColor = (EditText) findViewById(R.id.edtColor);
+
+        btnAddMartialArt = (Button) findViewById(R.id.btnAddMartialArt);
+        btnBack = (Button) findViewById(R.id.btnGoBack);
+
+
     }
 }
